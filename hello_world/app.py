edf4619c5b30330320cfdc2234bc20ce3413312e
Vijay Singh Kushwaha <vijaysinghkushwaha3737@gmail.com>
2024-02-13 18:33:26 +0000

    Optimizing image size

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,33 +1,12 @@
 import json
+import ollama
+import subprocess
 
 
+command = "ollama serve & sleep 1 && ollama run stablelm2:1.6b-q2_K"
+subprocess.run(command, shell=True, check=True, text=True)
 def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-            }
-        ),
-    }
+    message = json.loads(event['body'])['message']
+    response = ollama.generate(model="stablelm2:1.6b-zephyr-q2_K",prompt=message)
+    print(response)
+    return response["response"]
